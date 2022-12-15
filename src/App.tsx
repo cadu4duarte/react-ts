a605d6a699951b53ba32e5511ee09cd7fe445e20
Carlos Eduardo <cadu4duarte@gmail.com>
2022-12-15 20:46:28 -0300

    feat: interface/type props test

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,54 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
 
+type TypographyProps = {
+  children: React.ReactNode;
+  size?: "small" | "large";
+};
+
+type ParagraphProps = {
+  color: string;
+};
+
+const Paragraph = ({ children, size, color }: TypographyProps & ParagraphProps) => {
+  return (
+    <h1 
+      style={{
+        fontSize: size == "small" ? "1.5rem" : "3rem",
+      }}
+    >
+      {children}
+    </h1>
+  );
+};
+
+const Title = ({ children, size }: TypographyProps) => {
+  return(
+    <h1
+      style={{
+        fontSize: size == "small" ? "1.5rem" : "3rem",
+      }}
+    >
+      {children}
+    </h1>
+  );
+};
+
+const titleDefaultProps = {
+  size: "small"
+};
+
+Title.defaultProps = titleDefaultProps;
+
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Title>
+        <span>
+          Hello <b>World</b>
+        </span>
+      </Title>
     </div>
   )
 }
